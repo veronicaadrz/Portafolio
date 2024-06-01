82013abe712ca26937770a33d256ae0452703616
Verónica Alemán <147893988+veronicaadrz@users.noreply.github.com>
2024-05-31 19:47:20 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Desarrolladora Front-End.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
